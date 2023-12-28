47c8379011c36d57d444bbdca6c612f7c6075d6e
JyngJ <kaijjy@kaist.ac.kr>
2023-12-28 20:14:07 +0900

    MainActivity.kt 코드 긁어와서 바꿈

--- app/src/main/java/com/example/tab_pj/MainActivity.kt
+++ app/src/main/java/com/example/tab_pj/MainActivity.kt
@@ -16,32 +16,58 @@ class MainActivity : AppCompatActivity() {
         val PhotoFragment: Fragment = PhotoFragment()
         val ExtraFragment: Fragment = ExtraFragment()
 
+        supportFragmentManager.beginTransaction().replace(R.id.tabContent, NumFragment).commit()
 
-        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-
-            // 탭 버튼을 선택할 때 이벤트
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                val transaction = supportFragmentManager.beginTransaction()
-                when(tab?.text) {
-                    "tab1" -> transaction.replace(R.id.tabContent, NumFragment )
-                    "tab2" -> transaction.replace(R.id.tabContent, PhotoFragment )
-                    "tab3" -> transaction.replace(R.id.tabContent, ExtraFragment )
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when(tab.position) {
+                    0 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.tabContent, NumFragment).commit()
+                    }
+                    1 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.tabContent, PhotoFragment).commit()
+                    }
+                    2 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.tabContent, ExtraFragment).commit()
+                    }
                 }
-                transaction.commit()
-
-
             }
 
-            // 다른 탭 버튼을 눌러 선택된 탭 버튼이 해제될 때 이벤트
             override fun onTabUnselected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
             }
 
-            // 선택된 탭 버튼을 다시 선택할 때 이벤
             override fun onTabReselected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
             }
+
         })
     }
-    }
-
+}
+//
+//        supportFragmentManager.beginTransaction().replace(R.id.tabContent, NumFragment)
+//        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+//
+//            // 탭 버튼을 선택할 때 이벤트
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                val transaction = supportFragmentManager.beginTransaction()
+//                transaction.replace(R.id.tabContent, NumFragment)
+//                when(tab?.text) {
+//                    "tab1" -> transaction.replace(R.id.tabContent, NumFragment )
+//                    "tab2" -> transaction.replace(R.id.tabContent, PhotoFragment )
+//                    "tab3" -> transaction.replace(R.id.tabContent, ExtraFragment )
+//                }
+//                transaction.commit()
+//            }
+//
+//            // 다른 탭 버튼을 눌러 선택된 탭 버튼이 해제될 때 이벤트
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            // 선택된 탭 버튼을 다시 선택할 때 이벤
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//                TODO("Not yet implemented")
+//            }
+//        })
+//    }
+//    }
+//
