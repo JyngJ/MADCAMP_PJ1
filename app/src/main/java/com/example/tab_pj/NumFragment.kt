a5e175f9882b739c6ba125d537f1adf6a3b64525
JyngJ <kaijjy@kaist.ac.kr>
2023-12-29 16:52:48 +0900

    탭 둘다 완료

--- app/src/main/java/com/example/tab_pj/NumFragment.kt
+++ app/src/main/java/com/example/tab_pj/NumFragment.kt
@@ -5,12 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class NumFragment : Fragment () {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_num, container, false)
+        val view = inflater.inflate(R.layout.fragment_num, container, false)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerview_num)
+        val layoutManager = GridLayoutManager(requireContext(), 1)
+        recyclerView.layoutManager = layoutManager
+        val adapter = MyAdapter_num()
+        recyclerView.adapter = adapter
+
+        return view
     }
 }
