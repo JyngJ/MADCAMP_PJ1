c2a94e87b214657066dd2753d15de970278a3292
rabbitonthemoon <lkpnthr@gmail.com>
2023-12-30 13:31:36 +0900

    동그라미 버튼 만듦 !

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.code.gson:gson:2.8.5")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
