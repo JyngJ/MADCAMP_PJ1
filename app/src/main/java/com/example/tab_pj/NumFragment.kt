326ef8d648d9cbeb46a4798a392409055c84e7db
JyngJ <kaijjy@kaist.ac.kr>
2023-12-30 13:29:46 +0900

    Tab1 클릭시에 팝업 나오고 텍스트 필드, 저장 버튼 나옴

--- app/src/main/java/com/example/tab_pj/NumFragment.kt
+++ app/src/main/java/com/example/tab_pj/NumFragment.kt
@@ -1,15 +1,23 @@
 package com.example.tab_pj
 
+import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.json.JSONObject
 
-class NumFragment : Fragment () {
+
+
+class NumFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,6 +31,34 @@ class NumFragment : Fragment () {
         adapter.setDataFromJson(requireContext(), "Num.json")
         recyclerView.adapter = adapter
 
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            showDialog()
+        }
+
         return view
     }
+
+    private fun showDialog() {
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.popup_number_input)
+
+        val firstTextField = dialog.findViewById<EditText>(R.id.namefield)
+        val secondTextField = dialog.findViewById<EditText>(R.id.numberfield)
+        val saveButton = dialog.findViewById<Button>(R.id.saveButton)
+
+        saveButton.setOnClickListener {
+            val firstText = firstTextField.text.toString()
+            val secondText = secondTextField.text.toString()
+//
+//            val jsonData = createJsonData(firstText, secondText)
+//            saveJsonToFile(jsonData, "myData.json")
+
+            dialog.dismiss() // 다이얼로그 닫기
+        }
+
+        dialog.show()
+    }
+
+    // ... createJsonData와 saveJsonToFile 함수 ...
 }
--- app/src/main/java/com/example/tab_pj/PhotoFragment.kt
+++ app/src/main/java/com/example/tab_pj/PhotoFragment.kt
@@ -3,6 +3,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -10,7 +11,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.tab_pj.MyAdapter
 import com.example.tab_pj.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-class PhotoFragment : Fragment () {
+import org.json.JSONObject
+
+class PhotoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,23 +26,7 @@ class PhotoFragment : Fragment () {
         val adapter = MyAdapter()
         recyclerView.adapter = adapter
 
-        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener {
-            showDialog()
-        }
 
         return view
-    }
-
-    private fun showDialog() {
-        val dialog = Dialog(requireContext())
-        dialog.setContentView(R.layout.popup_number_input)
-
-        val firstTextField = dialog.findViewById<EditText>(R.id.namefield)
-        val secondTextField = dialog.findViewById<EditText>(R.id.numberfield)
-
-        // 여기서 버튼 클릭 리스너를 설정하여 텍스트 필드의 값을 사용할 수 있음
-
-        dialog.show()
     }
 }
